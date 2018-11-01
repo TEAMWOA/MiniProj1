86afba95cf927c9953d220b99c322234d2854e93
OJ <abdulaze@ualberta.ca>
2018-10-31 22:10:27 -0600

    cancel bookings done. only need messaging

--- main_file.py
+++ main_file.py
@@ -8,6 +8,15 @@ def db_exit():
     connection.commit()
     connection.close()
     quit()
+    
+def connect(path):
+    global connection, cursor
+
+    connection = sqlite3.connect(path)
+    cursor = connection.cursor()
+    cursor.execute('PRAGMA foreign_keys=ON;')
+    connection.commit()
+    return
 
 
 def login():
@@ -48,6 +57,10 @@ def register():
     # Otherwise returns False
 
     # Get and validate email
+    
+    # make email global so we can access it 
+    global email
+    
     email = input("\nEmail: ").lower()
     while len(email) > 15:
         email = input("\nEmail (15 Character Limit): ").lower()
@@ -77,18 +90,9 @@ def register():
 
         global user
         user = email
-
+        
+        
         return True
-
-
-def connect(path):
-    global connection, cursor
-
-    connection = sqlite3.connect(path)
-    cursor = connection.cursor()
-    cursor.execute('PRAGMA foreign_keys=ON;')
-    connection.commit()
-    return
 
 
 def post_ride_request():
@@ -134,7 +138,7 @@ def ride_search():
 
             # a list of sequences for each possible match
 
-            keyword = (keyword, keyword, keyword, keyword, keyword, keyword, keyword, keyword, keyword, keyword, keyword)
+            keyword = (keyword, keyword, keyword, keyword, keyword, keyword, keyword, keyword, keyword, keyword, keyword,)
 
             # execute query for each keyword
 
@@ -203,6 +207,170 @@ def ride_search():
     return True
 
 
+
+def add_booking():
+    
+    # list all rides offered by the user then select a ride to book a member
+    # on
+    #
+    # username is the only match, and only show future rides
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()   
+    
+    user = 'joe@gmail.com'
+    user = (user,)
+    
+    # retrieve all FUTURE rides offered by the user
+    cursor.execute("SELECT distinct r.rno, r.price, r.rdate, r.seats, r.lugDesc,r.src, r.dst, r.driver, r.cno FROM rides r WHERE driver = ? and rdate > date('now')", user)
+    
+    # fetch the ride matches
+    ride_matches = cursor.fetchall()
+    ride_numbers = []
+    
+    #  if there are none, tell the user, and ask if they want to offer a ride
+    if not ride_matches:
+        prompt = input("\nYou Have Not Offered Any Rides\nDo You Want To Offer A Ride?\nYes/No: ")
+        if prompt.strip() == 'yes':
+            offer_ride()
+        elif prompt.strip() == 'no':
+            return False
+    
+    # Print all the ride matches 
+    # make a list of ride_numbers for easier reference
+    else:
+        
+        print("\nHere Are Your Future Rides: \n")
+        for each in ride_matches:
+            print(*each)
+            ride_numbers.append(each["rno"])
+            
+    
+    # User picks a ride they want to book on         
+    choice = (input("\nEnter The Ride Number To Book or Enter to see more: "))
+    
+    
+    #will only accept integer for ride number matches
+    while not choice.isdigit():
+        choice = (input("\nEnter The Ride Number To Book or Enter to see more: "))
+        
+    #Check if the ride_number belongs to one of the users    
+    for each in ride_matches:
+        while int(choice) not in ride_numbers:
+            choice = input("\nInvalid ride number, try again: ")
+            
+    ride_num = int(choice)
+    
+    #Check if the chosen ride is out of seats    
+    cursor.execute("SELECT seats FROM rides where rno = ? ",(ride_num,))
+    seats_available = cursor.fetchone()[0]
+    print("\nRide Number: ", ride_num)
+    print("\nSeats Available: ", seats_available)
+    
+    # if it is full, return a message asking to proceed
+    if seats_available == 0:
+        proceed = ("\nThis ride is full! Proceed?\n(yes/no)").strip()
+        if proceed == 'no':
+            add_booking()
+        elif proceed == 'yes':
+            pass
+   
+    #If there are available seats, create new bookings number
+    # and start collecting booking data    
+    elif seats_available > 0:
+        cursor.execute("SELECT MAX(bno) from bookings")
+        old_bno = cursor.fetchone()[0]
+        new_bno = old_bno + 1
+        
+        #to check for a valid user
+        member_to_book = input("\nEnter member's email to book: ").strip()
+        while not valid_user(member_to_book):
+            member_to_book = input("\nInvalid Email, Enter another email: ").strip()
+        
+        #Get how many seats to book for the member
+        #Check that they input an int only
+        seats_to_book =input("\nHow many seats are being booked? ").strip()
+        while not seats_to_book.isdigit():
+            seats_to_book = input("\nEnter the number of seats being booked: ")
+        
+        
+        # If we are over the seat limit
+        if int(seats_to_book) > seats_available:
+            prompt = input("\nAttempting to overbook! Proceed?\n(yes/no): ")
+            if prompt == 'no':
+                add_booking()
+            elif prompt == 'yes':
+                seats_to_book = int(seats_to_book)
+        
+        #recieve the cost per seat and make sure a digit is put in
+        # then change it into a float before putting in db
+        
+        cost_per_seat = input("\nEnter the cost per seat: ")
+        while not cost_per_seat.isdigit():
+            
+            cost_per_seat = input("\nEnter the cost per seat: ")
+        
+        #recieve the pickup code, then check for validity
+        pickup = input("\nEnter the pickup location code: ")
+    
+        while not valid_lcode(pickup):
+            pickup = input("\nInvalid location code, try again: ")
+        
+        # Receieve the dropff code, then check for validity
+        dropoff = input("\nEnter the dropoff location code: ")
+        while not valid_lcode(dropoff):
+            dropoff = input("\nInvalid location code, try again: ")
+        
+        #The list of the new booking info    
+        new_booking = (new_bno, member_to_book, choice , cost_per_seat, seats_to_book, pickup, dropoff,)
+        
+        # GEt final prompt from user
+        prompt = input("\nConfirm Booking? (yes/no):   ").strip()
+        if prompt == "yes":
+        # Insert the new bookings into the table
+            cursor.execute("INSERT INTO BOOKINGS VALUES(?,?,?,?,?,?,?)", new_booking)
+            seats_left = (int(seats_available) - int(seats_to_book))
+        
+            # Then update the rides table 
+            updated_ride = (seats_left, ride_num,)
+            cursor.execute("UPDATE rides SET seats = ? where rno = ?", updated_ride)
+            
+            # after booking, prompt to make another one
+            prompt = input("\nBooking Confirmed. Would you like to make another one?\n(yes/no): ").strip()
+            if prompt == "yes":
+                add_booking()
+            elif prompt == "no":
+                logged_in_loop()
+        elif prompt == "no":
+            add_booking()
+        
+
+       
+    return True
+def valid_user(member_to_book):
+    valid = False
+    user = (member_to_book,)
+    cursor.execute("SELECT name from members where email = ? ",user)
+    if cursor.fetchone():
+        valid = True
+        
+    return valid
+
+def valid_lcode(lcode):
+    valid = False
+    location = (lcode,)
+    cursor.execute("SELECT address from locations where lcode = ? ", location)
+    if cursor.fetchone():
+        valid = True
+    return valid
+
+def cancel_booking():
+    
+    # Retreive all bookings on rides offered by current user and cancel
+    # specified booking(s)
+    
+    return
+
+
 def login_loop():
     while True:
         print("\nPlease choose an option:")
@@ -234,12 +402,13 @@ def logged_in_loop():
         print("\nPlease choose an option:")
         print("1. Post Ride Request")
         print("2. Search for Rides")
-        print("3. Logout")
-        print("4. Quit")
+        print("3. Create/Cancel Bookings")
+        print("4. Logout")
+        print("5. Quit")
 
         choice = input()
 
-        while choice not in ("1", "2", "3", "4"):
+        while choice not in ("1", "2", "3", "4", "5"):
             choice = input()
 
         if choice == "1":
@@ -247,11 +416,14 @@ def logged_in_loop():
 
         elif choice == "2":
             ride_search()
-
+        
         elif choice == "3":
-            login_loop()
+            add_booking()
 
         elif choice == "4":
+            login_loop()
+
+        elif choice == "5":
             db_exit()
 
 
@@ -259,7 +431,7 @@ def main():
     path = "./rideshare.db"
     connect(path)
 
-    login_loop()
+    add_booking()
 
     db_exit()
 
